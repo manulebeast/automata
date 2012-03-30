c7097f9f4508d10e05c2b0f54fdc6c0dfcdd9e98
Administrador <Administrador@Vista-SPa1n>
2012-03-30 04:25:40 -0600

    Agregacion de nuevo metodo para asumir nuevo estado, de aceptacion o de error!

--- src/javaapplication3/automataApp.java
+++ src/javaapplication3/automataApp.java
@@ -19,12 +19,28 @@ public class automataApp {
         matrizTrans[1][0] = 4; matrizTrans[1][1] = 3; matrizTrans[1][2] = -1;
         matrizTrans[2][0] = -1; matrizTrans[2][1] = -1; matrizTrans[2][2] = -1;
         matrizTrans[3][0] = 3; matrizTrans[3][1] = -1; matrizTrans[3][2] = -1;
-        estado = 1;
+        estado = -1;
+    }
+    
+    private void asumiendoValores(int val){
+        estado = matrizTrans[estado][val]; 
+        switch(estado){
+            case -1:{
+                System.out.println("Error!");
+                break;
+            }
+            case 3:{
+                System.out.println("Refresco!");
+                break;
+            }
+        }
+            
     }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
     }
 }
