5b17b6a8b78cbec538168c79fe43c142d20d763e
Administrador <Administrador@Vista-SPa1n>
2012-03-30 04:16:36 -0600

    Inicialización de variables por emdio del constructor

---
+++ src/javaapplication3/automataApp.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author Administrador
+ */
+public class automataApp {
+    
+    protected int[][] matrizTrans;
+    protected int estado;
+
+    public automataApp(){
+        //Inicializar variables
+        matrizTrans[0][0] = 1; matrizTrans[0][1] = 4; matrizTrans[0][2] = -1;
+        matrizTrans[1][0] = 4; matrizTrans[1][1] = 3; matrizTrans[1][2] = -1;
+        matrizTrans[2][0] = -1; matrizTrans[2][1] = -1; matrizTrans[2][2] = -1;
+        matrizTrans[3][0] = 3; matrizTrans[3][1] = -1; matrizTrans[3][2] = -1;
+        estado = 1;
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
